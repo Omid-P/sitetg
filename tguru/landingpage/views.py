e5776c2f4225914833d5bd5de2e49c45cd0b87a0
alex-hh <alexhooker@Alexs-MacBook-Pro-2.local>
2015-09-21 13:42:49 +0100

    Setup email form submission to database table

--- tguru/landingpage/views.py
+++ tguru/landingpage/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from landingpage.models import Email
+from django.utils import timezone
 
 # Create your views here.
 def landing(request):
     #return HttpResponse("hello")
     if request.method == 'POST':
+        q = Email(address=request.POST['email'], signup_date=timezone.now())
+        q.save()
         return HttpResponse("Thanks for signing up!")
     else:
         return render(request, 'landingpage/landing.html')
