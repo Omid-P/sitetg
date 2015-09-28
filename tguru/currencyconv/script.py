abfc2d94d9750732f10617eff0171246b17bf097
alex-hh <alexhooker@Alexs-MacBook-Pro-2.local>
2015-09-28 14:23:44 +0100

    "Currency converter updates"

--- tguru/currencyconv/script.py
+++ tguru/currencyconv/script.py
@@ -1,18 +1,37 @@
 __author__ = 'alexhooker'
-import json
-import urllib2
-def CurrencyConverter(currency_from, currency_to, amount):
-    yql_base_url = "https://query.yahooapis.com/v1/public/yql"
-    yql_query = 'select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20("'+currency_from+currency_to+'")'
-    yql_query_url = yql_base_url + "?q=" + yql_query + "&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
-    try:
-        yql_response = urllib2.urlopen(yql_query_url)
-        try:
-            yql_json = json.loads(yql_response.read())
-            currency_output = amount * float(yql_json['query']['results']['rate']['Rate'])
-            Error='NULL'
-            return currency_output
-        except:
-            return
-    except:
-        return
+import requests
+import MySQLdb
+#def currencylist():
+#    r = requests.get('http://openexchangerates.org/currencies.json')
+#    currencies = r.json()
+#for key in currencies.keys():
+#    cur.execute("INSERT INTO currency_list (code, name) VALUES('%s', '%s')" %(key, currencies[key]))
+
+
+
+#con = MySQLdb.connect(host="localhost",user="root",passwd="tgpass",db="tguru")
+# cur.execute("INSERT INTO Writers(Name) VALUES('Jack London')")
+#INSERT INTO tbl_name (col1,col2) VALUES(15,col1*2);
+# cur.execute('select * from currency_list')
+# cur.fetchall()
+# con.commit()
+# con.close()
+
+
+#import json
+#import urllib2
+#def CurrencyConverter(currency_from, currency_to, amount):
+#    yql_base_url = "https://query.yahooapis.com/v1/public/yql"
+#    yql_query = 'select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20("'+currency_from+currency_to+'")'
+#    yql_query_url = yql_base_url + "?q=" + yql_query + "&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
+#    try:
+#        yql_response = urllib2.urlopen(yql_query_url)
+#        try:
+#            yql_json = json.loads(yql_response.read())
+#            currency_output = amount * float(yql_json['query']['results']['rate']['Rate'])
+#            Error='NULL'
+#            return currency_output
+#        except:
+#            return
+#    except:
+#        return
--- tguru/currencyconv/views.py
+++ tguru/currencyconv/views.py
@@ -1,21 +1,58 @@
 from django.shortcuts import render
-from currencyconv.script import CurrencyConverter
+#from currencyconv.script import CurrencyConverter
 import json
 import urllib2
 from django.http import HttpResponse
+import requests
+import MySQLdb
+
+def get_currencies(db_connection):
+    cursor = db_connection.cursor()
+    cursor.execute('select * from currency_list order by name;')
+    currencylist = cursor.fetchall()
+    return currencylist
+
+def currencyconverter(currency_from, currency_to, amount):
+    yql_base_url = "https://query.yahooapis.com/v1/public/yql"
+    yql_query = 'select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20("'+currency_from+currency_to+'")'
+    yql_query_url = yql_base_url + "?q=" + yql_query + "&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
+    try:
+        yql_response = urllib2.urlopen(yql_query_url)
+        try:
+            yql_json = json.loads(yql_response.read())
+            currency_output = amount * float(yql_json['query']['results']['rate']['Rate'])
+            Error='NULL'
+            return currency_output
+        except:
+            return
+    except:
+        return
+
+
 # Create your views here.
 def convert(request):
     #return HttpResponse("hello")
-    if request.method == 'POST':
+    if request.method == 'POST' and request.is_ajax():
         yql_base_url = "https://query.yahooapis.com/v1/public/yql"
-        yql_query = 'select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20("'+request.POST['CurrencyFrom']+request.POST['CurrencyTo']+'")'
+        yql_query = 'select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20("'+request.POST['currencyFrom']+request.POST['currencyTo']+'")'
         yql_query_url = yql_base_url + "?q=" + yql_query + "&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
         yql_response = urllib2.urlopen(yql_query_url)
         yql_json = json.loads(yql_response.read())
         result = float(request.POST['amount']) * float(yql_json['query']['results']['rate']['Rate'])
-        return HttpResponse("%f %s = %f %s" % (float(request.POST['amount']),request.POST['CurrencyFrom'],float(result), request.POST['CurrencyTo']))
+       # return HttpResponse("%f %s = %f %s" % (float(request.POST['amount']),request.POST['currencyFrom'],float(result), request.POST['currencyTo']))
+        return HttpResponse(json.dumps({'res': result}, content_type="application/json"))
+    elif request.method == 'POST':
+        yql_base_url = "https://query.yahooapis.com/v1/public/yql"
+        yql_query = 'select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20("'+request.POST['currencyFrom']+request.POST['currencyTo']+'")'
+        yql_query_url = yql_base_url + "?q=" + yql_query + "&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
+        yql_response = urllib2.urlopen(yql_query_url)
+        yql_json = json.loads(yql_response.read())
+        result = float(request.POST['amount']) * float(yql_json['query']['results']['rate']['Rate'])
+        return HttpResponse("%f %s = %f %s" % (float(request.POST['amount']),request.POST['currencyFrom'],float(result), request.POST['currencyTo']))
     else:
-        return render(request, 'currencyconv/currencycalc.html')
+        db_connection = MySQLdb.connect(host="localhost",user="root",passwd="tgpass",db="tguru")
+        currencies = get_currencies(db_connection)
+        return render(request, 'currencyconv/currencycalc.html', {'currencies': currencies})
     #return render(request, 'landingpage/landing.html')
 
 
